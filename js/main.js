84f52386a0182240cd8bf1989fb4a263b2f23d76
Kim Mattiko <amberlie@rocketmail.com>
2015-06-16 08:22:02 -0400

    just beginning :

--- js/main.js
+++ js/main.js
@@ -6,4 +6,8 @@ JS I need based on my PLANS
   document images need to change when hovered to titles (model/view)
   I need something saved when info is added to form
 
+
+
+Reveal on hover on .journals section
+
 */
