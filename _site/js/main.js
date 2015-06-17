2336ae269bf1f6a8d0e772a7142e0de64fdd4808
Kim Mattiko <amberlie@rocketmail.com>
2015-06-17 12:03:19 -0400

    following along from class

---
+++ _site/js/main.js
@@ -0,0 +1,13 @@
+/*
+JS I need based on my PLANS
+
+  top page needs to rotate the adjs, sismilar to quotes rotator? (controller?)
+  Menu needs to track where the user scrolls on the page (model/view)
+  document images need to change when hovered to titles (model/view)
+  I need something saved when info is added to form
+
+
+
+Reveal on hover on .journals section
+
+*/
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+    browserSync = require('browser-sync').create();
+    sass = require('gulp-sass');
+
+gulp.task('serve', [ 'sass' ], function(){
+  browserSync.init({
+    server:"./_site"
+  });
+
+gulp.watch("scss/*.scss", [ 'sass' ]);
+gulp.watch("./_site/*.html")
+  .on('change', browserSync.reload)
+});
+
+gulp.task('sass',function(){
+  return gulp.src("scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
+});
